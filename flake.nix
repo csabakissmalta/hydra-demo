d701d896f48678ebb10f31cdedd5b38253de076a
csabakissmalta <jabakir@gmail.com>
2025-05-03 23:27:47 +0200

    demo

--- flake.nix
+++ flake.nix
@@ -1,27 +1,6 @@
-{
-  description = "A very basic flake";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.aarch64-linux;
-    in
-    {
-
-      packages.aarch64-linux.tree = nixpkgs.legacyPackages.aarch64-linux.tree;
-
-      defaultPackage.aarch64-linux = self.packages.aarch64-linux.tree;
-
-      hydraJobs."tester2" = self.defaultPackage;
-      hydraJobs."tester" = self.defaultPackage;
-      hydraJobs."tester-readme" = pkgs.runCommand "readme" { } ''
-        echo hello worl
-        mkdir -p $out/nix-support
-        echo "# A readme" > $out/readme.md
-        echo "doc readme $out/readme.md" >> $out/nix-support/hydra-build-products
-      '';
-    };
+{ ... }:
+let
+  pkgs = (import <nixpkgs> {});
+in {
+  hello = pkgs.hello;
 }
-
